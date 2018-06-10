413b34cc9e6074d056ae9967c860ee01c8b6c5e4
manchoydotid <nurmanchoy@gmail.com>
2018-06-10 16:48:45 +0700

    Replace EditText to SearchView

--- app/src/main/java/com/example/manchoy/booklisting/MainActivity.java
+++ app/src/main/java/com/example/manchoy/booklisting/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.manchoy.booklisting;
 
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -15,11 +12,10 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
-    
-    String keyword;
-    private EditText wordEditText;
-    private Button searchButton;
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    String query;
+    private SearchView searchView;
     private BookAdapter mAdapter;
     private static final String API_KEY = "AIzaSyBWZxUFeGeZ6_DJJZVHGmXNm1VX2h0VT10";
 
@@ -29,36 +25,36 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wordEditText = (EditText)findViewById(R.id.editText);
-        searchButton = (Button)findViewById(R.id.search_btn);
-
-        searchButton.setOnClickListener(myListener);
+        searchView = (SearchView)findViewById(R.id.sV);
+        searchView.setOnQueryTextListener(this);
 
         ListView bookListView = (ListView)findViewById(R.id.list);
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
         bookListView.setAdapter(mAdapter);
 
-
     }
 
-    View.OnClickListener myListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+    @Override
+    public boolean onQueryTextSubmit(String s) {
 
-            if(view.getId()==R.id.search_btn){
-                keyword = wordEditText.getText().toString();
-                if(keyword.contains(" ")){
-                    keyword = keyword.replace(" ","+");
-                }
-                Log.e("Kata yang dicari ", keyword);
-                String URL =
-                        "https://www.googleapis.com/books/v1/volumes?q="+keyword+"&key="+API_KEY;
-
-                BookAsyncTask task = new BookAsyncTask();
-                task.execute(URL);
-            }
+        if(s.contains(" ")){
+            query = s.replace(" ","+");
         }
-    };
+        Log.e("Kata yang dicari ", query);
+        String URL =
+                "https://www.googleapis.com/books/v1/volumes?q="+query+"&key="+API_KEY;
+
+        BookAsyncTask task = new BookAsyncTask();
+        task.execute(URL);
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
+    }
+
 
     private class BookAsyncTask extends AsyncTask<String, Void, List<Book>>{
 
